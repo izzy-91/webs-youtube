8ba35527001d93da132b7b1e04c483352d3a99ed
izzy-91 <www.izzy.xoxo@gmail.com>
2024-11-06 20:00:34 +0900

    메인 콘텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
     return (
         <footer id="footer" role="contentinfo">
-            Footer
+            <a href="mailto:sdjj91@gmail.com" rel="noopener noreferrer">
+                sdjj91@gmail.com
+            </a>
         </footer>
     );
 };
