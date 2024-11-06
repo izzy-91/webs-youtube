e5a44a9ff3c44eaade8458e1752bf317893fb7a0
izzy-91 <www.izzy.xoxo@gmail.com>
2024-11-06 19:13:04 +0900

    페이지 SEO 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,11 @@ import Main from "../components/section/Main";
 
 
 const Home = () => {
-    return <Main>Home</Main>;
+    return (
+        <Main title="웹스토리보이 유튜브" description="웹스토리보이 유튜브에 오신것을 환영합니다.">
+            Home
+        </Main>
+    );
 }
 
 export default Home;
