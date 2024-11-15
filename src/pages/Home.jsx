ad2f3d94018bc869b7207b941c03c15a225e05aa
izzy-91 <www.izzy.xoxo@gmail.com>
2024-11-15 18:19:25 +0900

    Swiper 이미지 슬라이드 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Main from "../components/section/Main";
 import Today from "../components/contents/Today";
 import Developer from "../components/contents/Developer";
 import VideoSlider from "../components/video/VideoSlider";
+
+import { todayText } from "../data/today";
+import { developerText } from "../data/developer";
 import { webdText } from "../data/webd";
 import { websiteText } from "../data/website";
 import { gsapText } from "../data/gsap";
@@ -12,8 +15,8 @@ import { youtubeText } from "../data/youtube";
 const Home = () => {
     return (
         <Main title="웹스토리보이 유튜브" description="웹스토리보이 유튜브에 오신것을 환영합니다.">
-            <Today />
-            <Developer />
+            <Today videos={todayText} id="today" />
+            <Developer videos={developerText} title="😪 추천 개발자를 소개합니다." id="developer" />
             <VideoSlider videos={webdText} title="😮 웹디자인기능사 준비는 이걸로!" id="webd" />
             <VideoSlider videos={websiteText} title="😛 웹표준 사이트 만들기 기초 다지기" id="website" />
             <VideoSlider videos={gsapText} title="🤓 GSAP 패럴랙스 효과를 하고 싶다면!" id="gsap" />
